344e855b760a60f2abc0c0a2319ca0dc0a63646e
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-07-31 17:06:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,33 @@
 # bot.py
-import telebot
+import requests  
+import os
+from flask import Flask, request
+# Add your telegram token as environment variable
+BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'
 
-bot = telebot.TeleBot("BOT_KEY")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+app = Flask(__name__)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
 
-bot.polling()
+@app.route('/', methods=['POST'])
+def main():  
+    data = request.json
+
+    print(data)  # Comment to hide what Telegram is sending you
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
+
+    json_data = {
+        "chat_id": chat_id,
+        "text": message,
+    }
+
+    message_url = BOT_URL + 'sendMessage'
+    requests.post(message_url, json=json_data)
+
+    return ''
+
+
+if __name__ == '__main__':  
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
