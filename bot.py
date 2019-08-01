2fb5579c6d7cc7501e3fe5eb184d5f352d09cab0
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-08-01 09:19:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,25 +10,25 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
-def main():  
+def main():
     data = request.json
 
     print(data)  # Comment to hide what Telegram is sending you
     chat_id = data['message']['chat']['id']
     message = data['message']['text']
 	
-	if data['text'] == "hola":
-	json_data = {
-	"chat_id": chat_id,
-	# "text": message,
-	"text": "Hola bienvenido",
-	}
+    if message == "hola":
+    json_data = {
+    "chat_id": chat_id,
+    # "text": message,
+    "text": "Hola bienvenido",
+    }
 
-	else:
-	json_data = {
-	"chat_id": chat_id,
-	"text": message,
-	}
+    else:
+    json_data = {
+    "chat_id": chat_id,
+    "text": message,
+    }
 
     message_url = BOT_URL + 'sendMessage'
     requests.post(message_url, json=json_data)
@@ -39,4 +39,5 @@ def main():
 if __name__ == '__main__':  
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
