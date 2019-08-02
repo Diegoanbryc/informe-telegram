017c390fa4c78f82ba294310086e5385004560e3
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-08-02 16:55:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # bot.py
-import consultaexterna.php
+
 import subprocess
 import requests  
 import os
@@ -22,7 +22,7 @@ def main():
     if message == "Informe" or message == "informe":
         print("Entro al if")
         json_data = {"chat_id": chat_id, "text": "En el laboratorio RYC se encuentran trabajos de: ",}
-       # subprocess.call(["php", "./consultaexterna.php"])
+        subprocess.call(["php", "./consultaexterna.php"])
     elif message == "Hola" or message =="hola":
         json_data = {"chat_id": chat_id, "text": "Hola, Por favor escriba la palabra: Informe, para dar el informe de trabajos presentes en el laboratorio",}
     else:
