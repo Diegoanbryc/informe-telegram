590eace37ab6a0f0eb998a86116a9d39b4c5dfa5
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-08-15 15:59:03 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,6 +72,9 @@ def main():
         message_url = BOT_URL + 'sendMessage'
         requests.post(message_url, json=json_data)
         
+    elif message.startswith( '/20' )
+        fechaconsulta = datetime.strptime(message, '/%y_%m_%d')
+        print("Va a consultar los trabajos con fecha de:", fechaconsulta)
     else:
         json_data = {"chat_id": chat_id, "text": "Por favor escriba la palabra: Informe, para dar el informe de trabajos presentes en el laboratorio",}
         message_url = BOT_URL + 'sendMessage'
