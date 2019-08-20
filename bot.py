73b4a012b780bfae2e2be7f754085381dd030da3
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-08-20 14:31:33 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,9 +94,9 @@ def main():
             print("Nr Orden = ", row2[1])
             print("Gaveta = ", row2[2])
             print("Estado  = ", row2[3], "\n")
-           json_data = {"chat_id": chat_id, "text": "|   "+str(row2[0])+"      | "+str(row2[1])+"   |  "+str(row2[2])+"   |  "+str(row2[3])+"      |",}
-           message_url = BOT_URL + 'sendMessage'
-           requests.post(message_url, json=json_data)
+            json_data = {"chat_id": chat_id, "text": "|   "+str(row2[0])+"      | "+str(row2[1])+"   |  "+str(row2[2])+"   |  "+str(row2[3])+"      |",}
+            message_url = BOT_URL + 'sendMessage'
+            requests.post(message_url, json=json_data)
         
         
         
