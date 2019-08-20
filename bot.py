44843b5f68505e8dca69cdede438c7d22e55b4ad
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-08-20 12:16:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import os
 from datetime import datetime
 from flask import Flask, request
 MySQLdb.paramstyle
+'%s'
 # Add your telegram token as environment variable
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'
 
