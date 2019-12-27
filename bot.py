33694c66cb774184bc11c45c79bb88c8c4370f40
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2019-12-27 09:45:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,8 +69,7 @@ def main():
             message_url = BOT_URL + 'sendMessage'
             requests.post(message_url, json=json_data)
             #time.sleep(3)
-             return ''
-            cursor.close()
+
         json_data = {"chat_id": chat_id, "text": "Los días de atraso no tienen en cuenta ni Sábados ni Domingos, Ahora selecciona la fecha de realizado el cálculo, para obtener información de los trabajos de esa fecha.",}
         message_url = BOT_URL + 'sendMessage'
         requests.post(message_url, json=json_data)        
@@ -105,8 +104,7 @@ def main():
             json_data = {"chat_id": chat_id, "text": "|   "+str(row2[0])+"    | "+str(row2[1])+" |  "+str(row2[2])+"  | "+str(row2[3])+"      |",}
             message_url = BOT_URL + 'sendMessage'
             requests.post(message_url, json=json_data)
-            return ''
-            cursor.close()
+
             
         json_data = {"chat_id": chat_id, "text": "Regresa y selecciona otra fecha o escribe la palabra informe, para mirar de nuevo el listado de informe general.",}
         message_url = BOT_URL + 'sendMessage'
