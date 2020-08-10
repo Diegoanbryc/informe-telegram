ef6c42d0b32a73d5c8bcdeeeb9cf22dab4d69dc7
Diegoanbryc <53495986+Diegoanbryc@users.noreply.github.com>
2020-08-10 13:33:47 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,6 +99,48 @@ def fechaconsultaDB(a):
       requests.post(message_url, json=json_data)
   cursor.close()
   
+  
+def OrdenconsultaDB(Orden):
+  data = request.json
+  chat_id = data['message']['chat']['id']
+  message = data['message']['text']
+  # Open database connection
+  db = MySQLdb.connect("ryclab.com","ryclabco","ryclab*+2015","ryclabco_wp557" )
+  # prepare a cursor object using cursor() method
+  # Check if connection was successful
+  if db:
+    # Carry out normal procedure
+      print("Connection successful")
+  else:
+      # Terminate
+      print("Connection unsuccessful")
+  cursor = db.cursor()
+  # execute SQL query using execute() method.
+  cursor.execute("SELECT VERSION();")
+  # Fetch a single row using fetchone() method.
+  data2 = cursor.fetchone()
+  print("Conecto a la base de datos externa:")
+  print(data2)
+  cursor.execute("SET lc_time_names = 'es_ES';")
+  cursor.execute("set session sql_mode='TRADITIONAL';")
+  sqlorden ="select NumCalculo, NumOrden, Gaveta, Estado FROM trabajos_lab where NumOrden like {0} AND date(fecha_calculado)>DATE_SUB(NOW(),INTERVAL 15 DAY)" 
+  cursor.execute(sqlorden.format(orden))
+  infofecha = cursor.fetchall()
+  # time.sleep(3)
+  json_data = {"chat_id": chat_id, "text": "|Cálculo   | Nr. Orden     | Gaveta  | Estado en el lab.| ",}
+  message_url = BOT_URL + 'sendMessage'
+  requests.post(message_url, json=json_data)
+  # time.sleep(3)
+  for row2 in infofecha:
+      print("Calculo = ", row2[0], )
+      print("Nr Orden = ", row2[1])
+      print("Gaveta = ", row2[2])
+      print("Estado  = ", row2[3], "\n")
+      json_data = {"chat_id": chat_id, "text": "|   "+str(row2[0])+"    | "+str(row2[1])+" |  "+str(row2[2])+"  | "+str(row2[3])+"      |",}
+      message_url = BOT_URL + 'sendMessage'
+      requests.post(message_url, json=json_data)
+  cursor.close()
+  
 @app.route('/', methods=['POST'])
 def main():
     data = request.json
@@ -140,6 +182,20 @@ def main():
         message_url = BOT_URL + 'sendMessage'
         requests.post(message_url, json=json_data)
         
+    elif message == "Consulta Orden"
+        json_data = {"chat_id": chat_id, "text": "Para consultar el estado de una Orden, Por favor escriba la palabra: \"Orden\" y a continuación el número de Orden a consultar",}
+        message_url = BOT_URL + 'sendMessage'
+        requests.post(message_url, json=json_data)
+        
+    elif message.startswith( 'Orden ' ):
+        mensaje5a = menssage.find("Orden ")
+        Orden=print(mensaje5[5:])
+        print("Información sobre la orden número: ", Orden)
+        json_data = {"chat_id": chat_id, "text": "A continuación se muestran la información relacionada al número de Orden"+Orden+": ",}
+        message_url = BOT_URL + 'sendMessage'
+        requests.post(message_url, json=json_data)
+        OrdenconsultaDB(Orden)
+
         
         
     else:
